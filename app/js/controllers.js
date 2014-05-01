6f12ced90416dd9de1e6f424cfa43046a9ac26b1
liuxiong <liuxiong332@163.com>
2014-05-01 21:14:18 +0800

    add the cookies support

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,25 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('logupControl', ['$scope', function($scope) {
+angular.module('myApp.controllers', ['ngCookies'])
+  .controller('logupControl', ['$scope','$cookies','$http',
+   function($scope,$cookies,$http) {
     $scope.isLoginActive = true;
     $scope.isLogupActive = false;
+    //get the userName from the cookie
+    $scope.userName = $cookies.userName;
+    $scope.isUserExists = !!$cookies.userName;
+
     //when click the login and logup button invoked
     $scope.onMenuClick = function() {
       $scope.isLoginActive = !$scope.isLoginActive;
       $scope.isLogupActive = !$scope.isLogupActive;
     };
+    // $scope.onLogin = function() {
+    //   $http.post('/login',{userName:})
+    // };
+    // $scope.onLogup = function() {
+    //   onMenuClick();
+    // };
+
   }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,12 +3,14 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(module('ngCookies'));
+  beforeEach(angular.mock.module('myApp.controllers'));
 
-
-  it('should active the specific menu item', inject(function($controller) {
+  it('should active the specific menu item', inject(
+    function($httpBackend, $cookies, $controller) {
     var scope = {};
-    var myCtrl1 = $controller('logupControl',{$scope: scope});
+    var myCtrl1 = $controller('logupControl',
+      {$scope: scope});
     expect(myCtrl1).toBeDefined();
     scope.onMenuClick();
     expect(scope.isLoginActive).toBe(false);
@@ -18,5 +20,4 @@ describe('controllers', function(){
     expect(scope.isLoginActive).toBe(true);
     expect(scope.isLogupActive).toBe(false);
   }));
-
 });
